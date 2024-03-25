2844369b1d8863a26814e1ab1870d126cfc51d23
Divyanshu <2573divyanshu@gmail.com>
2024-03-25 23:34:58 +0530

    day2

---
+++ practice.cpp
@@ -0,0 +1,336 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+// reverse on string belongs to the algorithm header file
+// reverse string using reverse(str.begin(),str.end());
+// length of a string using str.length();
+// to_string() from the string header file
+// sqrt() from the cmath
+// maps header file for map
+
+// COMPLEXITY OF HASHING USING UNORDERED_MAPS
+// storing TC is O(1) (average and best)
+// fetch TC is O(1) (average and best)
+// in worst case O(n)
+// use unordered_map if it give TLE then use map
+
+
+
+// COMPLEXITY OF HASHING USING MAPS
+// storing and fetching TC O(log(n)) (in all cases, best, average and worst)
+
+// HASHIN USING MAPS (CHARACTER HASHIN)
+// int main(){
+//     string s;
+//     cout<<"Enter the string: ";
+//     cin>>s;
+//     unordered_map<char,int> map1;
+//     for(int i = 0; i<s.length(); i++)
+//     {
+//         map1[s[i]]++;
+//     };
+//     // char c;
+//     // while(true)
+//     // {
+//     //     cin>>c;
+//     //     if(c=='z')
+//     //     {
+//     //         break;
+//     //     }
+//     //     else
+//     //     {
+//     //         cout<<c<<" occurred "<<map1[c]<<" times"<<endl;
+//     //     };
+//     // };
+//     // iteratoring over the map
+//     for(auto it : map1)
+//     {
+//         cout<<it.first<<" occurred "<<it.second<<endl;
+//     };
+// };
+
+// HASHING USING MAPS (INTEGER HASHING)
+// int main(){
+//     int n;
+//     cout<<"Enter the size: ";
+//     cin>>n;
+//     int arr[n];
+//     map<int,int> map1;
+//     for(int i = 0; i<n; i++)
+//     {
+//         cin>>arr[i];
+//         map1[arr[i]]++;
+//     };
+//     int num;
+//     while(true)
+//     {
+//         cin>>num;
+//         if(num==-1)
+//         {
+//             break;
+//         }
+//         else
+//         {
+//             cout<<num<<" occured "<<map1[num]<<" times"<<endl;
+//         };
+//     }
+// };
+// it's TC is O(n);
+
+// MAP STL
+// concept of key value pairs
+// keys are unique
+// int main()
+// {
+//     // declaration
+//     map<char, int> map1;
+//     // insertion way 1
+//     map1['a'] = int('a');
+//     map1['b'] = int('b');
+//     map1['c'] = int('c');
+//     // insertion way 2 (using the insert function)
+//     map1.insert({'d', int('d')});
+//     // accessing
+//     cout << map1['a'] << endl;
+//     cout << map1['b'] << endl;
+//     cout << map1['c'] << endl;
+//     cout << map1['d'] << endl;
+//     // iterating
+//     for (auto it : map1)
+//     {
+//         cout << it.first << " " << it.second << " " << endl;
+//     };
+//     cout << endl;
+//     // finding
+//     auto it = map1.find('a');
+//     cout<<it->second<<endl;
+// };
+
+// PROBLEMS WITH ARRAY HASHING
+// is it won't work if the size of input is too large
+// solution is hash_map or map
+
+// HASHING USING ARRAYS (character hashin)
+// int main()
+// {
+//     string s;
+//     cout<<"Enter the string: ";
+//     cin>>s;
+//     // this will hash for all characters
+//     // int arr[255] = {0};
+//     // this will hash for alphbet characters
+//     int arr[25] = {0};
+//     for(int i = 0; i<s.length(); i++)
+//     {
+//         arr[s[i]-'a']++;
+//     };
+//     char c;
+//     while(true)
+//     {
+//         cin>>c;
+//         if(c=='z')
+//         {
+//             break;
+//         }
+//         else
+//         {
+//             cout<<c<<" occurred "<<arr[c-'a']<<" times"<<endl;
+//         };
+//     };
+// };
+
+// HASHING USING ARRAYS (integer hashing)
+// int main(){
+//     int n;
+//     cout<<"Enter the size of the array: ";
+//     cin>>n;
+//     int arr[n];
+//     int max = INT32_MIN;
+//     cout<<"Enter the elements: ";
+//     for(int i = 0; i<n; i++)
+//     {
+//         cin>>arr[i];
+//         if(arr[i]>max)
+//         {
+//             max = arr[i];
+//         };
+//     };
+//     int frekArr[max+1] = {0};
+//     for(int i = 0; i<n; i++)
+//     {
+//         frekArr[arr[i]]++;
+//     };
+//     int num = 0;
+//     while(true)
+//     {
+//         cin>>num;
+//         if(num==-1)
+//         {
+//             break;
+//         };
+//         cout<<num<<" occurred "<<frekArr[num]<<" times"<<endl;
+//     };
+// };
+// limitations:
+// inside the main function the maximum size of the array that we can declare is 10^7, outside main it's 10^7
+// for boolean the values are 10^7 and 10^8
+
+// HASHING BRUTEFORCE
+// the bruteforce method
+// int frek(int n, vector<int> vect)
+// {
+//     int count = 0;
+//     for(int i = 0; i<vect.size(); i++)
+//     {
+//         if(vect[i]==n)
+//         {
+//             count++;
+//         }
+//     };
+//     return count;
+// };
+// int main(){
+//     vector<int> vect{1,2,2,3,3,3,4,4,4,4,5,5,5,5,5,};
+//     cout<<frek(1, vect)<<endl;
+//     cout<<frek(2, vect)<<endl;
+//     cout<<frek(3, vect)<<endl;
+//     cout<<frek(4, vect)<<endl;
+//     cout<<frek(5, vect)<<endl;
+// };
+// this has a TC of O(Q*n)
+// where n is the size of the vector
+// Q is the number of distinct integers present
+
+// GCD/HCF
+// int calcGCD(int n, int m)
+// {
+//     while (n > 0 && m > 0)
+//     {
+//         if (n > m)
+//         {
+//             n %= m;
+//         }
+//         else
+//         {
+//             m %= n;
+//         };
+//     };
+//     if (n == 0)
+//     {
+//         return m;
+//     }
+//     else
+//     {
+//         return n;
+//     };
+// };
+// int main()
+// {
+//     cout << calcGCD(20, 10) << endl;
+// };
+// TC is O(log(min(a,b))) (base phi)
+
+// CHECK PRIME
+// one way is to loop till n this will lead to TC O(n)
+// another way is to loop till sqrt(n) this will lead to O(sqrt(n))
+
+// PRINT ALL DIVISORS
+// void printAllDivs(int n)
+// {
+//     // for (int i = 1; i <= sqrt(n); i++)
+//     for (int i = 1; i*i<=n; i++)
+//     {
+//         if (n % i == 0)
+//         {
+//             cout << i << " ";
+//             if (n / i != i)
+//             {
+//                 cout << n/i << " ";
+//             }
+//         };
+//     };
+// };
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     printAllDivs(n);
+// };
+// TC of this alsgo is O(sqrt(N))
+
+// CONVERTING NUMBER TO STRING
+// int main()
+// {
+//     int n = 123;
+//     string s = to_string(n);
+//     cout<<s<<endl;
+// }
+
+// EXTRACTION OF DIGITS FROM A NUMBER
+// one way
+// int main()
+// {
+//     int n;
+//     cout<<"Enter the value of n: ";
+//     cin>>n;
+//     while(n>0)
+//     {
+//         int digit = n%10;
+//         cout<<digit<<" ";
+//         n/=10;
+//     };
+// };
+// TC for this method is O(log10(n))
+// second way
+// int main(){
+//         int n;
+//         cout<<"Enter the value of n: ";
+//         cin>>n;
+//         cout<<"Number of digits: "<<(int)(log10(n)+1)<<endl;
+// };
+
+// TLEs
+// most of them do 10^8 operations in 1 second
+
+// SPACE COMPLEXITY
+// rate at which space occuped increases wrt to input is called the TC
+// total space = auxiliary space + input space
+// aux space is space taken to solve the problem
+// input space is the space to store the input
+// never change the input data unless asked to
+
+// TIME COMPLEXITY
+// rate at which time taken increases wrt to input is called the TC
+// TC is measured in O(n) (Big O Notation)
+// rules:
+// - always calculate worst case TC
+// - avoid constants
+// - ignore lower values
+// int main()
+// {
+//     int N;
+//     cin>>N;
+//     for(int i = 0; i<N; i++)
+//     {
+//         cout<<"Hi"<<endl;
+//     };
+// };
+// this code has TC of O(n);
+// worst case using Big O Notation (upper bound)
+// best case using Theta Notation
+// average case using Omega Notation (lower bound)
+// O(4N^3 + 3N^2 + 8) ~ O(N^3)
+
+// PI
+// get pi's value using this
+// but first include the the cmath header file
+// int main()
+// {
+//     cout<<M_PI<<endl;
+//     return 0;
+// };
---
+++ stl.cpp
@@ -0,0 +1,154 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void iterateVector(vector<int> &v)
+{
+    for(auto it : v)
+    {
+        cout<<it<<" ";
+    };
+    cout<<endl;
+};
+
+// stopped at 31.54
+
+// ⚡ vectors
+// problem with arrays: we cannot change their size in runtime
+// but the size of vectors can be changed in the run time
+// doubt: emplace back is not working
+// even if we have declared the size of the vector, the vector can be expanded
+// doubt: push front isn't working
+// int main(){
+//     // example 1
+//     // vector<int> v1;
+//     // v1.push_back(1);
+//     // v1.push_back(2);
+//     // v1.push_back(3);
+//     // iterateVector(v1);
+//     // example 2
+//     // vector<pair<int,int>>v1;
+//     // v1.push_back({1,2});
+//     // v1.push_back({3,4});
+//     // cout<<v1[0].first<<endl;
+//     // cout<<v1[0].second<<endl;
+//     // cout<<v1[1].first<<endl;
+//     // cout<<v1[1].second<<endl;
+//     // example 3
+//     // vector<int> v(5,2);
+//     // iterateVector(v);
+//     // vector<int> vcopy(v);
+//     // iterateVector(vcopy);
+//     // example 4
+//     // vector<int> v1(3,2);
+//     // iterateVector(v1);
+//     // v1.push_back(3);
+//     // v1.push_back(4);
+//     // v1.push_back(5);
+//     // iterateVector(v1);
+//     // example 5 (iterating an array using iterator)
+//     // vector<int> v1 = {1,2,3,4,5};
+//     // for(vector<int>::iterator it = v1.begin(); it != v1.end(); it++)
+//     // {
+//     //     cout<<*(it)<<" ";
+//     // };
+//     // cout<<endl;
+//     // example 6 (using iterators)
+//     // vector<int> v1 = {1,2,3,4,5};
+//     // vector<int>::iterator it = v1.begin();
+//     // cout<<*(it++)<<endl;
+//     // cout<<*(it++)<<endl;
+//     // cout<<*(it++)<<endl;
+//     // cout<<*(it++)<<endl;
+//     // cout<<*(it++)<<endl;
+//     // example 7 (some more iterators)
+//     // vector<int> v1 = {1,2,3,4,5};
+//     // vector<int>::iterator it1 = v1.end();
+//     // cout<<*(it1)<<endl;
+//     // cout<<*(--it1)<<endl;
+//     // end points to somewhere after the last element
+//     // example 8 (back)
+//     // vector<int> v1 = {1,2,3,4,5};
+//     // cout<<v1.back()<<endl;
+//     // example 9 (iterating through a vectors using iterators)
+//     // vector<int> v1 = {1,2,3,4,5};
+//     // for(auto it = v1.begin(); it!=v1.end(); it++)
+//     // {
+//     //     cout<<*(it)<<endl;
+//     // };
+//     // example 10 (short way to iterator over a vector)
+//     // vector<int> vect = {1,2,3,4,5};
+//     // for(auto it : vect)
+//     // {
+//     //     cout<<it<<" ";
+//     // };
+//     // example 11 (deletion in a vector)
+//     // vector<int> vect = {1,2,3,4,5};
+//     // iterateVector(vect);
+//     // vect.erase(vect.begin(),vect.begin()+1);
+//     // iterateVector(vect);
+//     // // give two arguments if you want to specify the start position and the end position
+//     // vect.erase(vect.begin()+1);
+//     // iterateVector(vect);
+//     // give one argument if you want to specify only one position
+//     // example 12 (insertion in a vector)
+//     // vector<int> vect = {1,2,3,4,5};
+//     // iterateVector(vect);
+//     // vect.insert(vect.begin(),9);
+//     // iterateVector(vect);
+//     // vect.insert(vect.begin(),2,10);
+//     // insertion 2 10s at vect.begin()
+//     // iterateVector(vect);
+//     // example 13 (inserting the contents of a vector into another vector)
+//     // vector<int> vect1 = {1,2,3,4,5};
+//     // vector<int> vect2 = {11,12,13,14,15};
+//     // iterateVector(vect1);
+//     // vect1.insert(vect1.begin(),vect2.begin(),vect2.end());
+//     // iterateVector(vect1);
+//     // example 14 (getting the size of a vector)
+//     // vector<int> vect = {1,2,3,4,5};
+//     // cout<<vect.size();
+//     // example 15 (swapping a vector)
+//     // vector<int> vect1 = {1,2,3};
+//     // vector<int> vect2 = {4,5,6};
+//     // iterateVector(vect1);
+//     // iterateVector(vect2);
+//     // swap(vect1,vect2);
+//     // iterateVector(vect1);
+//     // iterateVector(vect2);
+//     // example 16 (empty check)
+//     // vector<int> vect1 = {1,2,3};
+//     // vector<int> vect2 = {};
+//     // cout<<vect1.empty()<<endl;
+//     // cout<<vect2.empty()<<endl;
+//     // example 17 (push_front function)
+//     vector<int> vect1 = {1,2,3};
+//     iterateVector(vect1);
+//     iterateVector(vect1);
+// };
+
+// ⚡ pairs
+// int main()
+// {
+//     // example 1
+//     // pair<int,int> p = {1,3};
+//     // cout<<"first element is "<<p.first<<endl;
+//     // cout<<"second element is "<<p.second<<endl;
+//     // example 2
+//     // pair<int, pair<int,int>> p = {1,{2,3}};
+//     // cout<<"first element is "<<p.first<<endl;
+//     // cout<<"second pair is "<<p.second.first<<" "<<p.second.second<<endl;
+//     // example 3
+//     pair<int,int> arr[] = {{1,2},{3,4},{5,6}};
+//     cout<<"first element of array is the pair "<<arr[0].first<<" "<<arr[0].second<<endl;
+//     cout<<"second element of array is the pair "<<arr[1].first<<" "<<arr[1].second<<endl;
+//     cout<<"third element of array is the pair "<<arr[2].first<<" "<<arr[2].second<<endl;
+
+//     return 0;
+// };
+
+// ⚡ one library for everythin
+// #include<bits/stdc++.h>
+
+// ⚡header files
+// - math.h
+// - string.h
